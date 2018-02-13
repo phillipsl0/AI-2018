865e32016a87e61cf443fea330ffa4b5fd03b0ec
Veronica Child <vachild@me.com>
2018-02-13 17:20:11 -0600

    Rough implementation of information gain function

--- osha_compliant/DataParser.py
+++ osha_compliant/DataParser.py
@@ -3,8 +3,8 @@ DataParser.py
 Assignment #3
 '''
 
-
-def parseData(fname):
+# Returns list of dictonaries, each dictionary is data associated with a heatmiser
+def parseDataJSON(fname):
     with open(fname, "r") as f:
         f.readline()  # skip header
         # Iterate line by line
@@ -23,20 +23,44 @@ def parseData(fname):
                 heatmiser[headers[i]] = lineList[i]
 
             data.append(heatmiser)
-
-    print(data)
     return data
 
+# Returns list of lists, each list is data associated with a heatmiser
+def parseDataList(fname):
+    with open(fname, "r") as f:
+        f.readline()  # skip header
+        # Iterate line by line
+        data = []
+        headers = ['ID', 'Distance', 'Speeding', 'Location', 'OSHA']
+        for line in f:
+            line = line.strip()
 
-def getData():
+            # Redo tabs with spaces
+            line = line.replace('\t', ',')
+
+            lineList = line.split(",")
+
+            heatmiser = []
+            # Only numbers for now
+            # for i in range(len(lineList)):
+            for i in range(1, 3):
+                heatmiser.append(lineList[i])
+            data.append(heatmiser)
+    return data
+
+def getDataList():
     fname = 'HW3_Data.txt'
-    data = parseData(fname)
+    data = parseDataList(fname)
     return data
 
+def getDataJSON():
+    fname = 'HW3_Data.txt'
+    data = parseDataJSON(fname)
+    return data
 
 def main():
     fname = 'HW3_Data.txt'
-    data = parseData(fname)
+    data = parseDataList(fname)
 
 
 if __name__ == '__main__':
